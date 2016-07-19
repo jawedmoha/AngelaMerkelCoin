72ee495d1e7eb5ce08dd9cd064e6e92cc6f1ec42
Merkel <Merkel>
2016-07-20 01:56:44 +0500

    initial commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define AngelaMerkelCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "AngelaMerkelCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
